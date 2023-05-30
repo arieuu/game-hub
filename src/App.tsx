65a5726d4a9cae0844bce446eed23d223ec87079
Pliskin <arieljanickcarvalho@gmail.com>
2023-05-30 10:02:55 -0100

    Built a responsive layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import { Button, Tab, Tabs, TabList, TabPanels, TabPanel } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 import './App.css'
 
 function App() {
-  return <Button colorScheme='blue'> Entrar </Button>
+  return <>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`, // bigger than 1024px
+    }}>
+
+      <GridItem area="nav" bg="coral"> Nav </GridItem>
+
+      <Show above='lg'>
+        <GridItem area="aside" bg="gold"> Aside </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue"> Main </GridItem>
+    </Grid> 
+  </>
 }
 
 export default App
