2b873f5d3d65a78dc30a297373d23d4780f47dc2
Pliskin <arieljanickcarvalho@gmail.com>
2023-05-30 10:46:49 -0100

    Built color mode switch.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,14 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/Logo/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 
 function NavBar() {
     return(
         <>
-            <HStack>
+            <HStack justifyContent="space-between" padding="12px">
                 <Image src={logo} boxSize="60px"/>
-                <Text> Company name </Text>
+                <ColorModeSwitch />
             </HStack> 
         </>
     )
