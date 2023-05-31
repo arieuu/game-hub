a69d462cd282503b71b33f4d9ee18b8299b99645
Pliskin <arieljanickcarvalho@gmail.com>
2023-05-31 16:20:06 -0100

    Showing loading skeletons.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -6,9 +6,6 @@ import apiClients from "../services/api-clients";
 import { CanceledError } from "axios";
 
 
-
-
-
 interface FetchGamesResponse {
     count: number,
     results: Game[]
@@ -36,23 +33,29 @@ function useGames() {
 
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const controller = new AbortController(); // Handling cancellation
 
+        setIsLoading(true);
         apiClients.get<FetchGamesResponse>("/games", {signal: controller.signal})
-        .then(res => setGames(res.data.results))
-
+        .then(res => {
+            setGames(res.data.results);
+            setIsLoading(false); // Updating state to hide loading cards, the content is ready to be displayed.
+        })
+        
         .catch(err => {
             if (err instanceof CanceledError) return; // Clean up in case of cancelled
-            setError(err.message)
+            setError(err.message);
+            setIsLoading(false); // Stop loading cards from showing
         });
 
         // Returning a clean up function
         return () => controller.abort();
     }, []);
 
-    return { games, error };
+    return { games, error, isLoading };
 
 }
 
