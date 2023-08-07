4ed57bc0cb3d8e4e63eae2dddce0f67f53e80b2c
Pliskin <arieljanickcarvalho@gmail.com>
2023-08-07 11:24:33 -0100

    Implemented infinite scroll.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,12 +1,11 @@
-import { Button, SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
-import { Platform } from "../hooks/usePlatforms";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenres"
 import { GameQuery } from "../App";
 import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
     gameQuery: GameQuery;
@@ -19,20 +18,34 @@ function GameGrid({ gameQuery }: Props) {
 
     if(error) return <Text> {error.message} </Text>
 
+    // Complicated math to determine the total number of items (games) we've fetched so far
+
+    const fetchedGamesCount = data?.pages.reduce(
+        (total, page) => total + page.results.length, 0
+    ) /* This is undefined in the start so we initialize it */ || 0
+
     return(
             <>
-                <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 3}} spacing={6} padding="10px">
-                    {/** Passing the card components as children to the container so that they get the general styling */}
+                <InfiniteScroll
+                    dataLength={fetchedGamesCount} 
+                    hasMore={!!hasNextPage} /* Convert value to boolean (undefined will become false) */
+                    next={() => fetchNextPage()}
+                    loader={<Spinner  marginLeft="50%" />}
+                >
 
-                    {isLoading && skeletons.map((skeleton) => <GameCardContainer key={skeleton}> <GameCardSkeleton/> </GameCardContainer> )}
+                    <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 3}} spacing={6} padding="10px">
+                        {/** Passing the card components as children to the container so that they get the general styling */}
 
-                    {data?.pages.map((page, index) => {
-                        return <React.Fragment key={index}>
-                            {page.results.map((game) => <GameCardContainer key={game.id}> <GameCard game={game}/> </GameCardContainer>)}
-                        </React.Fragment>
-                    })}
-                </SimpleGrid> 
-                {hasNextPage && <Button onClick={() => fetchNextPage()}> {isFetchingNextPage ? "Loading..." : "Load more"} </Button>}
+                        {isLoading && skeletons.map((skeleton) => <GameCardContainer key={skeleton}> <GameCardSkeleton/> </GameCardContainer> )}
+
+                        {data?.pages.map((page, index) => {
+                            return <React.Fragment key={index}>
+                                {page.results.map((game) => <GameCardContainer key={game.id}> <GameCard game={game}/> </GameCardContainer>)}
+                            </React.Fragment>
+                        })}
+
+                    </SimpleGrid> 
+                </InfiniteScroll>
             </>
     )
 
