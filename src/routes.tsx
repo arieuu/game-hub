f689612cf72f229d514ecf3a2e45b7c0e170d480
Pliskin <arieljanickcarvalho@gmail.com>
2023-08-17 10:23:40 -0100

    Handled routing errors.

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import GameDetailPage from "./pages/GameDetailPage";
 import Homepage from "./pages/HomePage";
 import Layout from "./pages/Layout";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
+        errorElement: <ErrorPage />,
         
         children: [
             { path: "", element: <Homepage/> },
