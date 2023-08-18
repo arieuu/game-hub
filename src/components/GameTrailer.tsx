31678e61ccc009430d969a57d4554f3b91455d35
Pliskin <arieljanickcarvalho@gmail.com>
2023-08-18 10:43:58 -0100

    Displaying game trailers

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,28 @@
+import useTrailers from "../hooks/useTrailers";
+
+interface Props {
+    gameId: number;
+}
+
+const GameTrailer = ({gameId}:Props) => {
+    const {data, isLoading, error} = useTrailers(gameId)
+    console.log(data)
+    if (isLoading) return null
+
+    if (error) throw error
+
+    const first = data?.results[0];
+
+    if (!first) return null;
+
+    return (
+        <video
+            src={first.data[480]}        
+            poster={first.preview}
+            controls
+        />
+
+    )
+}
+
+export default GameTrailer;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -5,6 +5,7 @@ import ExpandableText from "../components/ExpandableText";
 import DefinitionItem from "../components/DefinitionItem";
 import CriticScore from "../components/CriticScore";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 
 
 const GameDetailPage = () => {
@@ -20,6 +21,7 @@ const GameDetailPage = () => {
             <Heading>{ game.name }</Heading> 
             <ExpandableText>{game.description_raw}</ExpandableText>
             <GameAttributes game={game}/> 
+            <GameTrailer gameId={game.id} />
         </>
     )
 }
